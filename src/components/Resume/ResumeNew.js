e397aa2523cefc9cef569ffa39e3388b1c65165e
Vaibhav Sharma <vaibhavsharma11061998@gmail.com>
2026-03-02 10:25:29 +0530

    Small Change

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -4,7 +4,8 @@ import Button from "react-bootstrap/Button";
 import pdf from "../../Assets/../Assets/Resume-VaibhavSharma.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
-import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+import 'react-pdf/dist/Page/TextLayer.css';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function ResumeNew() {
